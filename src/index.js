74b5f39be004456e860be545057cc096c35f982f
heyanny2 <heyanny2@yandex.ru>
2022-09-19 17:33:33 +0300

    support yaml/yml format, parcers are in a separated module

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import fs from 'fs';
 import path from 'path';
-import compareData from './compareData.js'
+import compareData from './compareData.js';
 import diff from './formatters/stylish.js';
+import parse from './parsers.js'
 
 //getting file directory and transforming to absolute path
 const readFilePath = (filepath) => path.resolve(process.cwd(), filepath);
 //reading file by modifying to utf-8 format
 const readFile = (filepath) => fs.readFileSync(readFilePath(filepath), 'utf-8');
-//parsing file from string to object
-const parse = (file) => JSON.parse(file);
-//const getFormat = (filename) => filename.split('.')[1];
+const getFormat = (filename) => filename.split('.')[1];
 
 const genDiff = (file1, file2) => {
   const data1 = readFile(file1);
   const data2 = readFile(file2);
-  const parsed1 = parse(data1);//, getFormat(file1)
-  const parsed2 = parse(data2);//, getFormat(file2)
+  const parsed1 = parse(data1, getFormat(file1));
+  const parsed2 = parse(data2, getFormat(file2));
   const data = compareData(parsed1, parsed2);
 
   return diff(data);

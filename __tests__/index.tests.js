7850ea7e8e0b282d167ea816478abbc49df6fa17
heyanny2 <heyanny2@yandex.ru>
2022-10-06 16:39:44 +0300

    add plain formatter, tests for plain

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -9,8 +9,20 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('testing stylish for plain file', () => {
-  const expected = readFile('test2.txt');
+const options = [
+  ['file1.json', 'file2.json', 'StylishTestResult.txt', 'stylish'],
+  ['file1.yaml', 'file2.yml', 'StylishTestResult.txt', 'stylish'],
+  ['file1.json', 'file2.json', 'PlainTestResult.txt', 'plain'],
+  ['file1.yaml', 'file2.yml', 'PlainTestResult.txt', 'plain']
+];
+
+test.each(options)('testing different file options', (file1, file2, resultFile, format) => {
+  const actual = genDiff(getFixturePath(file1), getFixturePath(file2), format);
+  expect(actual).toBe(readFile(resultFile));
+});
+
+test('testing default option', () => {
+  const expected = readFile('StylishTestResult.txt');
   const actual = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
   expect(actual).toBe(expected);
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import compareData from './compareData.js';
-import diff from './formatters/stylish.js';
+import format from './formatters/index.js';
 import parse from './parsers.js'
 
 //getting file directory and transforming to absolute path
@@ -10,14 +10,14 @@ const readFilePath = (filepath) => path.resolve(process.cwd(), filepath);
 const readFile = (filepath) => fs.readFileSync(readFilePath(filepath), 'utf-8');
 const getFormat = (filename) => filename.split('.')[1];
 
-const genDiff = (file1, file2) => {
+const genDiff = (file1, file2, formatName = 'stylish') => {
   const data1 = readFile(file1);
   const data2 = readFile(file2);
   const parsed1 = parse(data1, getFormat(file1));
   const parsed2 = parse(data2, getFormat(file2));
   const data = compareData(parsed1, parsed2);
 
-  return diff(data);
+  return format(data, formatName);
 };
 
 export default genDiff;

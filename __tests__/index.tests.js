209ef28ef94009eb41a66dc5e5534c83b7ce3f70
heyanny2 <heyanny2@yandex.ru>
2022-10-31 11:52:13 +0300

    changed fixtures and test file

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -1,28 +1,25 @@
-import fs from 'fs';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import genDiff from '../src/index.js';
+import fs from "fs";
+import path from "path";
+import genDiff from "../src/index.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const fileExt = [".json", ".yml"];
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const resultStylish = fs.readFileSync(
+	path.resolve(process.cwd(), "__fixtures__/result_stylish.txt"),
+	"utf-8"
+);
+const resultPlain = fs.readFileSync(
+	path.resolve(process.cwd(), "__fixtures__/result_plain.txt"),
+	"utf-8"
+);
 
-const fileExt = ['.json', '.yaml',  '.yml'];
-const stylishResult = readFile('StylishTestResult');
-const plainResult = readFile('PlainTestResult');
-const jsonResult = readFile('jsonTestResult');
-
-test.each(fileExt)('testing different file options', (extension) => {
-  const fileBefore = getFixturePath(`fileBefore${extension}`);
-  const fileAfter = getFixturePath(`fileAfter${extension}`);
-  const actual1 = genDiff(fileBefore, fileAfter, 'stylish');
-  expect(actual1).toEqual(stylishResult);
-  const actual2 = genDiff(fileBefore, fileAfter, 'plain');
-  expect(actual2).toEqual(plainResult);
-  const actual3 = genDiff(fileBefore, fileAfter, 'json');
-  expect(actual3).toEqual(jsonResult);
-  const actual4 = genDiff(fileBefore, fileAfter);
-  expect(actual4).toEqual(stylishResult);
+test.each(fileExt)("testing different file options", (extension) => {
+	const fileBefore = `__fixtures__/file1${extension}`;
+	const fileAfter = `__fixtures__/file2${extension}`;
+	const actual1 = genDiff(fileBefore, fileAfter, "stylish");
+	expect(actual1).toEqual(resultStylish);
+	const actual2 = genDiff(fileBefore, fileAfter, "plain");
+	expect(actual2).toEqual(resultPlain);
+	const actual4 = genDiff(fileBefore, fileAfter);
+	expect(actual4).toEqual(resultStylish);
 });
